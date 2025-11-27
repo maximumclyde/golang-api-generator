88dfae02eaaf4f450decef0f371350ca98164f96
maximumclyde <maxhaku.uni@gmail.com>
2025-11-27 11:50:41 +0100

    added faker decorator

--- models/template.model.go
+++ models/template.model.go
@@ -1,7 +1,7 @@
 package models
 
 type Template struct {
-	Id string `json:"id" gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
+	Id string `json:"id" gorm:"type:uuid;default:gen_random_uuid();primaryKey" faker:"uuid_hyphenated"`
 }
 
 func (Template) TableName() string {
