c0e3a5afb35e210d5f0581ed976bf3e89587ceb1
maximumclyde <maxhaku.uni@gmail.com>
2025-12-03 12:07:42 +0100

    fixed bug in attach query conditions function

--- utils/attach_query_conditions.go
+++ utils/attach_query_conditions.go
@@ -17,7 +17,7 @@ with the condition as the first parameter and the bindings as the rest of the pa
 dbWithConditions := AttachQueryConditions(db, ["name = <> ? and created_at < ?", "Marc", "<some-date>"], model{Industry: "<some-industry>"})
 */
 func AttachQueryConditions(db *gorm.DB, query ...any) *gorm.DB {
-	if db == nil {
+	if db == nil || len(query) == 0 {
 		return db
 	}
 
@@ -25,9 +25,17 @@ func AttachQueryConditions(db *gorm.DB, query ...any) *gorm.DB {
 	for _, q := range query {
 		// if the type is a slice, we destructure the conditions for the update
 		if reflect.TypeOf(q).Kind() == 23 {
-			// we put the first argument as the main syntax for the query and destructure the bindings
 			qC := q.([]any)
-			conditions = conditions.Where(qC[0], qC[1:]...)
+			if len(qC) == 0 {
+				continue
+			}
+
+			// we put the first argument as the main syntax for the query and destructure the bindings
+			if len(qC) > 1 {
+				conditions = conditions.Where(qC[0], qC[1:]...)
+			} else {
+				conditions = conditions.Where(qC[0])
+			}
 		} else {
 			// in case we are not dealing with an array, we can simply add the condition to the query
 			conditions = conditions.Where(q)
